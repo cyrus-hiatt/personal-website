661d1d0f8ccd1ec2baca4075ea358eeba34e5544
cyrus-hiatt <cyhi@queue-it.com>
2024-11-06 11:18:44 +0100

    add loading spinner and footer styling

--- src/App.js
+++ src/App.js
@@ -1,63 +1,52 @@
 import "./styles/style.css";
 import "./styles/fonts.css";
-import React, { useEffect } from "react";
-import {
-  Routes,
-  Route,
-  Navigate,
-  useLocation, // Hook to track location changes
-} from "react-router-dom";
+import React, { useEffect, Suspense } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import i18n from "./i18n";
 import Nav from "./components/nav";
-import Home from "./components/home";
-import Projects from "./components/projects";
-import Experience from "./components/experience";
-import Contact from "./components/contact";
 import Footer from "./components/footer";
 import ThemeToggle from "./components/themetoggle";
-import NotFound from "./components/notfound";
+import Spinner from "./components/spinner";
+
+// Lazy load your components
+const Home = React.lazy(() => import("./components/home"));
+const Projects = React.lazy(() => import("./components/projects"));
+const Experience = React.lazy(() => import("./components/experience"));
+const Contact = React.lazy(() => import("./components/contact"));
+const NotFound = React.lazy(() => import("./components/notfound"));
 
 function App() {
-  const { t } = useTranslation();
-  const location = useLocation(); // Track location changes
-  const { i18n } = useTranslation(); // Access the i18n instance
+  const { i18n } = useTranslation();
+  const location = useLocation();
 
-  // Function to set language based on the current route
   const setLanguageFromPath = (path) => {
-    const lang = path.includes("/en")
-      ? "en"
-      : path.includes("/da")
-      ? "da"
-      : "en";
+    const lang = path.includes("/da") ? "da" : "en";
     i18n.changeLanguage(lang);
   };
 
   useEffect(() => {
-    // Change the language whenever the path changes
     setLanguageFromPath(location.pathname);
     document.documentElement.lang = i18n.language;
-  }, [location.pathname, i18n]); // This will trigger on every route change
+  }, [location.pathname, i18n]);
 
   return (
     <div className="app-container">
       <Nav />
       <div className="content">
-        <Routes>
-          {/* Redirect to /en if the root path is visited */}
-          <Route path="/" element={<Navigate to="/en" />} />
-          {/* For /en path */}
-          <Route path="/en" element={<Home language="en" />} />
-          <Route path="/en/projects" element={<Projects />} />
-          <Route path="/en/experience" element={<Experience />} />
-          <Route path="/en/contact" element={<Contact />} />
-          {/* For /da path */}
-          <Route path="/da" element={<Home language="da" />} />
-          <Route path="/da/projekter" element={<Projects />} />
-          <Route path="/da/erfaring" element={<Experience />} />
-          <Route path="/da/kontakt" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/en" />} />
+            <Route path="/en" element={<Home />} />
+            <Route path="/en/projects" element={<Projects />} />
+            <Route path="/en/experience" element={<Experience />} />
+            <Route path="/en/contact" element={<Contact />} />
+            <Route path="/da" element={<Home />} />
+            <Route path="/da/projekter" element={<Projects />} />
+            <Route path="/da/erfaring" element={<Experience />} />
+            <Route path="/da/kontakt" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </div>
       <ThemeToggle />
       <Footer />
